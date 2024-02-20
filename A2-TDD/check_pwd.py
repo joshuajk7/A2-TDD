52800d8f16cacd617fb6d9dbd7a35a109f306033
joshuajk7 <kimjo8@oregonstate.edu>
2024-02-19 17:10:43 -0800

    check_pwd passes and tests 5 and all test prior

--- A2-TDD/check_pwd.py
+++ A2-TDD/check_pwd.py
@@ -1,5 +1,16 @@
 def check_pwd(pwd):
     if len(pwd) > 7 and len(pwd) < 21:
-        return True
+        
+        lowercase_found = False
+
+        for char in pwd:
+            if char.islower():
+                lowercase_found = True
+                break
+        
+        return lowercase_found
+    
     else:
         return False
+
+    
